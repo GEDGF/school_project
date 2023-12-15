0f0e18e9cbeaa55cff8ba59daadc3405aedd70c8
Han <dan.gr8668@gmail.com>
2023-12-16 05:14:05 +0800

    all repetitors

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -8,6 +8,8 @@ class Repetitor(models.Model):
     photo = models.ImageField(blank=True, upload_to='images')
     education = models.CharField(max_length=200)
     experience = models.IntegerField()
+    specialty = models.CharField(blank=True, max_length=100)
+    cost = models.IntegerField(blank=True)
 
     def __str__(self):
         return self.name
